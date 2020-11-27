eabf6e8c43a07fd5c4d69cfa42963b33ed2ffa47
Alex Weeden <ajweeden2016@gmail.com>
2020-11-27 14:18:37 -0600

    Regression Training and Testing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_validate, train_test_split
 
 quandl.ApiConfig.api_key = 'U8bAyuBxWfoTb1LeVLGR'
 
@@ -15,6 +18,23 @@ df = df[['Adj_Close','HL_PCT','PCT_change','Adj_Volume']]
 forecast_col = 'Adj_Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.1*len(df)))
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test,y_test)
+
+print(accuracy)
 
 
